c406e46939e56163c89e793961a6cdda41934c37
floriano-delishaj <delishaj.floriano@live.it>
2022-02-01 17:28:12 +0100

    end filter genres + bonus (filter authors)

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,82 @@
 <template>
   <div id="app">
     <header-content />
-    <main-container v-if="apiLoad" :discs="discs"/>
-    <loader-content v-else/>
+    <select-bar @selectGenre="filterGenresResult"
+      @selectAuthor="filterArtistResult"
+      :genres="genresFiltered"
+      :artists="artistFiltered"
+    />
+    <main-container v-if="apiLoad" :discs="discFiltered" />
+    <loader-content v-else />
   </div>
 </template>
 
 <script>
-import axios from 'axios'
-
-import HeaderContent from './components/HeaderContent.vue'
-import MainContainer from './components/MainContainer.vue'
-import LoaderContent from './components/LoaderContent.vue'
-
+import axios from "axios";
+import HeaderContent from "./components/HeaderContent.vue";
+import MainContainer from "./components/MainContainer.vue";
+import LoaderContent from "./components/LoaderContent.vue";
+import SelectBar from "./components/SelectBar.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HeaderContent,
     MainContainer,
     LoaderContent,
+    SelectBar,
   },
   data() {
     return {
-      discs : [],
+      discs: [],
+      discFiltered: [],
+      genresFiltered: [],
+      artistFiltered: [],
       apiLoad: false,
+    };
+  },
+  methods: {
+    filterGenresResult(select) {
+      this.discFiltered = this.discs.filter((disk) => {
+        return disk.genre.includes(select);
+      });
+    },
+    filterArtistResult(select) {
+      this.discFiltered = this.discs.filter((disk) => {
+        return disk.author.includes(select);
+      });
     }
   },
   mounted() {
-    setTimeout( () => {
-      axios('https://flynn.boolean.careers/exercises/api/array/music')
-        .then((result) => {
-          this.discs = result.data.response
-          this.apiLoad = true
-      })
-    },2000)
-  }
-}
+    setTimeout(() => {
+      axios("https://flynn.boolean.careers/exercises/api/array/music").then(
+        (result) => {
+          this.discs = result.data.response;
+          this.discFiltered = this.discs;
+          this.apiLoad = true;
+
+          //GENRES
+
+          this.discs.forEach((e) => {
+            if (!this.genresFiltered.includes(e.genre)) {
+              this.genresFiltered.push(e.genre);
+            }
+          });
+
+          //AUTHOR
+
+          this.discs.forEach((e) => {
+            if (!this.artistFiltered.includes(e.author)) {
+              this.artistFiltered.push(e.author);
+            }
+          });
+        }
+      );
+    }, 2000);
+  },
+};
 </script>
 
 <style lang="scss">
-
-  @import '@/style/main.scss';
-
+@import "@/style/main.scss";
 </style>
---
+++ src/components/SelectBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <!-- GENRES -->
+    <select
+      v-model="selectedGenre"
+      @change="$emit('selectGenre', selectedGenre)"
+    >
+      <option disabled value="">Please choice genre</option>
+      <option value="">All</option>
+      <option v-for="(genre, i) in genres" :key="i" :value="genre">
+        {{ genre }}
+      </option>
+    </select>
+
+    <select
+      v-model="selectedArtist"
+      @change="$emit('selectAuthor', selectedArtist)"
+    >
+      <option disable value="">Please choice artist</option>
+      <option value="">All</option>
+      <option v-for="(artist, i) in artists" :key="i" :value="artist">
+        {{ artist }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedGenre: "",
+      selectedArtist: "",
+    };
+  },
+  props: {
+    genres: Array,
+    artists: Array,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
